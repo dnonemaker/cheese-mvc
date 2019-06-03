c8aefad6d274e72c8ac3bfd4bf155c7db0226041
David Nonemaker <davidnonemaker@gmail.com>
2019-06-03 18:31:52 -0500

    additional features

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseContoller.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseContoller.java
@@ -6,21 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 @Controller
 @RequestMapping("cheese")
 class CheeseController {
 
-    static  ArrayList<String> cheeses = new ArrayList<>();
+    // static  ArrayList<String> cheeses = new ArrayList<>();
+    private static HashMap<String, String>cheesesExtended = new HashMap<>();
 
     // request path: /cheese
     @RequestMapping(value="")
     public String index(Model model){
 
-        model.addAttribute("cheeses",cheeses);
+        //model.addAttribute("cheeses",cheeses);
+        model.addAttribute("cheesesExtended", cheesesExtended);
         model.addAttribute("title", "My Cheeses");
 
         return"cheese/index";
@@ -35,9 +37,11 @@ class CheeseController {
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
+    public String processAddCheeseForm(@RequestParam String cheeseName,
+                                       @RequestParam String description){
 
-        cheeses.add(cheeseName);
+        //cheeses.add(cheeseName);
+        cheesesExtended.put(cheeseName,description);
         // redirect to /cheese
         return "redirect:";
 
